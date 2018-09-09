871e6a37be4675728ca9170cb5e4a43110c8ba75
Shahrul Fz <shahrulefz@gmail.com>
2018-09-09 12:53:20 +0800

    Project Setup

---
+++ productsbackend/src/main/java/net/fz/productsbackend/App.java
@@ -0,0 +1,13 @@
+package net.fz.productsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
